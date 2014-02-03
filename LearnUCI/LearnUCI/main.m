f5ab733623536f107cc884cc6b0dcf1f49b7f2e3
App Jam <french5033@gmail.com>
2014-02-02 16:17:12 -0800

    first commit

---
+++ LearnUCI/LearnUCI/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LearnUCI
+//
+//  Created by App Jam on 2/2/14.
+//  Copyright (c) 2014 gbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
